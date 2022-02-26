08cec7cb7810224f69e4d9783b1d5ab0b30c81d5
Cortland Young <dcy.y@outlook.com>
2022-02-26 12:46:25 -0500

    object chapter exercises

--- obj_chapter/myArray.js
+++ obj_chapter/myArray.js
@@ -6,6 +6,9 @@ let myArray = {
   length: 4
 };
 
+// for this object to mimmic an array, must use ordered integers for keys to represent the index locations
+// also must add a lenght property for the conditional statement in for loop
+
 for (let i = 0; i < myArray.length; i += 1) {
   console.log(myArray[i]);
 }
