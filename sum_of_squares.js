48d4f1f04b63a6e47b563068cb653625112392b1
CortlandY <82116819+CortlandY@users.noreply.github.com>
2022-02-26 12:51:52 -0500

    Delete sum_of_squares.js

--- sum_of_squares.js
+++
@@ -1,9 +0,0 @@
-let array = [3, 5, 7];
-
-function sumOfSquares(arr){
-  return arr.reduce((accumulator, element) => {
-    return accumulator + (element * element);
-  }, 0);
-}
-
-console.log(sumOfSquares(array)); // => 83
