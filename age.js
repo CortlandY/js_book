f15009f266e4f579469d1049f8aef3082c004aa4
CortlandY <82116819+CortlandY@users.noreply.github.com>
2022-02-26 12:49:41 -0500

    Delete age.js

--- age.js
+++
@@ -1,8 +0,0 @@
-let rlSync = require('readline-sync');
-let age = Number(rlSync.question('How old are you?\n'));
-
-console.log(`You are ${age} years old.\n`);
-
-for (index = 1; index <= 4; index += 1){
-  console.log(`in ${index*10} years, you will be ${age + (index*10)} years old.\n`)
-}
